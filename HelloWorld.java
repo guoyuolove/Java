0ddc16337e071456e3e544a07a9ea4f41252d305
guoyuolove <guoyuo@hotmail.com>
2015-04-15 20:47:42 +0800

    Changed return type of my_strlen to size_t

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: HelloWorld.java
+	> Author: 
+	> Mail: 
+	> Created Time: 三  4/15 21:22:14 2015
+ ************************************************************************/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
